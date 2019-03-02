3af3372fb01ffcad2ee2cc782ec0b2472aac4f30
Milo Knowles <mknowles@mit.edu>
2019-03-02 18:13:29 -0500

    Ordering working

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def FindPageContour(img, resolution=(640, 480)):
+def FindPageContour(img, resolution):
   # Resize and convert to grayscale
   img = cv2.resize(img, resolution)
   img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -15,9 +15,6 @@ def FindPageContour(img, resolution=(640, 480)):
 
   # Median filter clears small details
   img = cv2.medianBlur(img, 11)
-
-  # cv2.imshow('debug', img)
-  # cv2.waitKey(0)
 
   # Add black border in case that page is touching an image border
   img = cv2.copyMakeBorder(img, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=[0, 0, 0])
@@ -61,25 +58,99 @@ def FindPageContour(img, resolution=(640, 480)):
   # Result in pageConoutr (numpy array of 4 points):
   return pageContour
 
+def order_points(pts):
+  # initialzie a list of coordinates that will be ordered
+  # such that the first entry in the list is the top-left,
+  # the second entry is the top-right, the third is the
+  # bottom-right, and the fourth is the bottom-left
+  rect = np.zeros((4, 2), dtype = "float32")
+ 
+  # the top-left point will have the smallest sum, whereas
+  # the bottom-right point will have the largest sum
+  s = pts.sum(axis = 1)
+  rect[0] = pts[np.argmin(s)]
+  rect[2] = pts[np.argmax(s)]
+ 
+  # now, compute the difference between the points, the
+  # top-right point will have the smallest difference,
+  # whereas the bottom-left will have the largest difference
+  diff = np.diff(pts, axis = 1)
+  rect[1] = pts[np.argmin(diff)]
+  rect[3] = pts[np.argmax(diff)]
+ 
+  # return the ordered coordinates
+  return rect
+
+def four_point_transform(image, pts):
+  # obtain a consistent order of the points and unpack them
+  # individually
+  rect = order_points(pts)
+  (tl, tr, br, bl) = rect
+ 
+  # compute the width of the new image, which will be the
+  # maximum distance between bottom-right and bottom-left
+  # x-coordiates or the top-right and top-left x-coordinates
+  widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+  widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+  maxWidth = max(int(widthA), int(widthB))
+ 
+  # compute the height of the new image, which will be the
+  # maximum distance between the top-right and bottom-right
+  # y-coordinates or the top-left and bottom-left y-coordinates
+  heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+  heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+  maxHeight = max(int(heightA), int(heightB))
+ 
+  # now that we have the dimensions of the new image, construct
+  # the set of destination points to obtain a "birds eye view",
+  # (i.e. top-down view) of the image, again specifying points
+  # in the top-left, top-right, bottom-right, and bottom-left
+  # order
+  dst = np.array([
+    [0, 0],
+    [maxWidth - 1, 0],
+    [maxWidth - 1, maxHeight - 1],
+    [0, maxHeight - 1]], dtype = "float32")
+ 
+  # compute the perspective transform matrix and then apply it
+  M = cv2.getPerspectiveTransform(rect, dst)
+  warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+ 
+  # return the warped image
+  return warped
+
 def GetRoi(img):
   original_resolution = img.shape[1], img.shape[0] # Note: shape is rows, cols!
-  print('Original resolution:', original_resolution)
 
   # Downsampled resolution.
-  resolution = (640, 480)
-  upsample_factor = original_resolution[0] / resolution[0]
-
-  print('Factor:', upsample_factor)
-
-  img_downsampled = cv2.resize(img, resolution)
-  outline = FindPageContour(img, resolution=resolution)
+  contour_resolution = (480, 640)
+  outline = FindPageContour(img, resolution=contour_resolution)
 
   # Scale the outline back to original resolution.
   # NOTE: must be integer coordinates to draw!
+  upsample_factor = original_resolution[0] / contour_resolution[0]
   outline_scaled = (upsample_factor * outline).astype(np.int32)
 
-  cv2.drawContours(img, [outline_scaled], 0, (0, 0, 255), 2)
+  corners = []
+  for i in range(outline_scaled.shape[0]):
+      corners.append(outline_scaled[i,:,:].reshape((2)))
+  corners = np.array(corners)
+
+  cv2.drawContours(img, [corners], 0, (0, 0, 255), 2)
   cv2.imshow('letter', img)
+  cv2.waitKey(0)
+
+  print(corners)
+
+  # Make birds-eye image.
+  pts_ordered = order_points(corners)
+  topview = four_point_transform(img, pts_ordered)
+
+  # Make the image a more manageable size.
+  crop_resolution = (480, 640)
+  img_downsampled = cv2.resize(topview, crop_resolution)
+
+  cv2.imshow('bev', topview)
   cv2.waitKey(0)
 
 # img = cv2.imread("/home/milo/Downloads/mail_640_480.png")
