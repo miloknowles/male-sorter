a425de8c6a343e86544b7d2b45c217b85765b00f
Isaac Perper <iperper@mit.edu>
2019-03-02 18:20:43 -0500

    Can flip and open mail door

---
+++ src/servo_test/servo_test.ino
@@ -0,0 +1,115 @@
+//#include <Servo.h>
+#include <VarSpeedServo.h>
+
+VarSpeedServo drop_motor;
+VarSpeedServo flip_motor;
+
+
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
+
+#define CLOSER_PIN 9
+static int DROP_OPEN = 10;
+static int DROP_CLOSED = 90;
+static int DROP_SPEED = 100;
+int drop_pos = 0;
+int drop_target_pos = DROP_CLOSED;
+
+#define FLIP_PIN 10
+static int FLIP_NORMAL = 60;
+static int FLIP_OPEN = 180;
+static int FLIP_SPEED = 255;
+int flip_pos = 0;
+int flip_target_pos = FLIP_NORMAL;
+bool flip = false;
+
+bool runProgram = false;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Arduino is ready");
+   
+  drop_motor.attach(CLOSER_PIN);  // attaches the servo on pin 9 to the servo object
+  flip_motor.attach(FLIP_PIN);
+
+  move_mail();
+  move_flip();
+  
+  
+}
+
+void loop() {
+  Serial.println("Program Started. Use 'o' to open, 'c' to close, 'f' to flip, 'q' to quit, 'r' to restart");
+  while (runProgram) {
+    handleSerial();
+    move_mail();
+    move_flip();
+    delay(15);
+  }
+  while (!runProgram) {
+    Serial.println("Program Stopped. Use 'r' to restart");
+    handleSerial();
+    delay(2000);
+  }
+}
+
+void handleSerial(){
+  while (Serial.available() > 0){
+    char incomingChar = Serial.read();
+    switch (incomingChar){
+      case 'o': //open
+        Serial.println("Opening Drop");
+        drop_target_pos = DROP_OPEN;
+      break;
+      
+      case 'c': //close
+        Serial.println("Closing Drop");
+        drop_target_pos = DROP_CLOSED;
+      break;
+
+      case 'f': //flip
+        Serial.println("Flipping");
+        flip = true;
+       break;
+
+//       case 'u': //unflip
+//        flip_target_pos = FLIP_NORMAL;
+//       break;
+      
+      case 'q': //quit
+        Serial.println("Shutting Down");
+        drop_target_pos = DROP_CLOSED;
+        runProgram = false;
+        Serial.println("Shut Down. Ok To Turn Off");
+      break;
+
+      case 'r': //restart or start
+        Serial.println("Starting Program");
+        runProgram = true;
+        Serial.println("Program Started");
+      break;
+    }
+  }
+}
+
+
+void move_mail(){
+  drop_motor.slowmove(drop_target_pos, DROP_SPEED);
+}
+
+void move_flip(){
+  if (flip && flip_motor.read() < FLIP_OPEN){
+    flip_motor.slowmove(FLIP_OPEN, FLIP_SPEED);
+//    Serial.println(flip_motor.read());
+  }
+  else{
+    flip = false;
+    flip_motor.write(FLIP_NORMAL);
+  }
+}
+
+
+  
